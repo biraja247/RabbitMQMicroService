82668bde680bb0ea43b03849ed1077d468090cf3
LAPTOP-5KGP72SA\Ripun <birajamohapatra.bbsr@gmail.com>
2020-06-28 19:21:59 +0530

    Added Bus Command And EventHandler

---
+++ MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    interface IEventBus
+    {
+        Task SendCommand<T>(T Command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>() where T : Event where TH : IEventHandler<T>;
+    }
+}
